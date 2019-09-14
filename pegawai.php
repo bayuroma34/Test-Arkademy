b5bb009443d1ed8ad5e382342f749cd88e3f8b13
bayuroma34 <47111412+bayuroma34@users.noreply.github.com>
2019-09-14 22:23:17 +0700

    Add files via upload

---
+++ pegawai.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+​
+<nav class="navbar navbar-expand-sm bg-light navbar-light shadow navbar-fixed">
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">ARKADEMY BOOTCAMP</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container" style="margin-top: 200px; flex:1; flex-direction: column;">
+    <div class="container-fluid" style="justify-content: center; display:flex; align-items:center; flex-direction:column">
+        <button class="btn btn-warning shadow" style="color: white; align-self:flex-end" type="submit">ADD</button>
+        <table>
+
+        </table>
+    </div>
+</div>
+
+<style type="text/css">
+    shadow{
+        -webkit-box-shadow: 10px 10px 21px -8px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 10px 21px -8px rgba(0,0,0,0.75);
+        box-shadow: 10px 10px 21px -8px rgba(0,0,0,0.75);
+    },
+</style>
